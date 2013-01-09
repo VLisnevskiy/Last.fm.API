50fcbafeebb014c080a2b8505937c3564f475b78
Vyacheslav Lisnevskyi <VLisnevskiy@gmail.com>
2013-01-09 09:07:28 -0800

    Implementing service methods from User section

---
+++ Last.fm.API/UserServices/TaggingType.cs
@@ -0,0 +1,23 @@
+﻿namespace Last.fm.API.UserServices
+{
+    /// <summary>
+    /// Tagging Type
+    /// </summary>
+    public enum TaggingType
+    {
+        /// <summary>
+        /// Tag for artist
+        /// </summary>
+        Artist,
+
+        /// <summary>
+        /// Tag for album
+        /// </summary>
+        Album,
+
+        /// <summary>
+        /// Tag for track
+        /// </summary>
+        Track
+    }
+}
