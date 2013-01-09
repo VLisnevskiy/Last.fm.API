8057cdfc9de1d7bf5eef11f5ab504d60c656ea5f
Vyacheslav Lisnevskyi <VLisnevskiy@gmail.com>
2013-01-09 09:08:28 -0800

    Implementing service methods from User section

---
+++ Last.fm.API/BaseLastFm/LastFmHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Last.fm.API.BaseLastFm
+{
+    /// <summary>
+    /// Helpers
+    /// </summary>
+    public static class LastFmHelpers
+    {
+        /// <summary>
+        /// Method for converting a System.DateTime value to a UNIX Timestamp
+        /// </summary>
+        /// <param name="value">date to convert</param>
+        /// <returns>UNIX Timestamp</returns>
+        public static double ConvertToTimestamp(this DateTime value)
+        {
+            //create Timespan by subtracting the value provided from
+            //the Unix Epoch
+            TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+            //return the total seconds (which is a UNIX timestamp)
+            return span.TotalSeconds;
+        }
+
+        /// <summary>
+        /// Method for converting a System.DateTime value to a UNIX Timestamp
+        /// </summary>
+        /// <param name="value">date to convert</param>
+        /// <returns>UNIX Timestamp</returns>
+        public static double ConvertToTimestamp(this DateTime? value)
+        {
+            if (value != null)
+            {
+                DateTime locValue = (DateTime)value;
+                //create Timespan by subtracting the value provided from
+                //the Unix Epoch
+                TimeSpan span = (locValue - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+                //return the total seconds (which is a UNIX timestamp)
+                return span.TotalSeconds;
+            }
+
+            return 0;
+        }
+    }
+}
